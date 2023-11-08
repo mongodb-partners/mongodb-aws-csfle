cbc5208f1354cab41f5e7bc6b00e638f341ca0f6
devadyut <devadyut@amazon.co.uk>
2023-11-08 10:02:08 +0000

    Version with Automatic Encryption Shared Library Code

--- csfle-service/encrypt.js
+++ csfle-service/encrypt.js
@@ -81,7 +81,6 @@ module.exports.createUniqueIndex = async () => {
 module.exports.createDataEncryptionKey = async () => {
     await getKeyManagementProviderDetails();
     const client = await mdb.get(false);
-    //await client.connect();
 
     const encryption = new ClientEncryption(client, {
         keyVaultNamespace,
@@ -92,12 +91,11 @@ module.exports.createDataEncryptionKey = async () => {
     });
     const dataEncryptionKey = key.toString("base64");
     console.log("DataKeyId [base64]: ", dataEncryptionKey);
-    //await client.close();
     return dataEncryptionKey;
 }
 
 const findKeyVaultCollectionExists = async () => {
-    const keyVaultClient = mdb.get(false);
+    const keyVaultClient = await mdb.get(false);
     const exists = await mdb.findCollectionExists(keyVaultClient, keyVaultDatabase, keyVaultCollection);
     console.log("Exists", exists);
     return exists;
